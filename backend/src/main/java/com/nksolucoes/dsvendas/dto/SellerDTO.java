c1fe0a9731d931df03f9dd8f564638bccecd6e0c
NicolasPires <nicolask.pires@gmail.com>
2021-11-08 01:49:39 -0300

    Layers

---
+++ backend/src/main/java/com/nksolucoes/dsvendas/dto/SellerDTO.java
@@ -0,0 +1,62 @@
+package com.nksolucoes.dsvendas.dto;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.nksolucoes.dsvendas.entities.Seller;
+
+public class SellerDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+
+	public SellerDTO() {
+
+	}
+
+	public SellerDTO(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public SellerDTO(Seller entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SellerDTO other = (SellerDTO) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
---
+++ backend/src/main/java/com/nksolucoes/dsvendas/service/SellerService.java
@@ -0,0 +1,25 @@
+package com.nksolucoes.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nksolucoes.dsvendas.dto.SellerDTO;
+import com.nksolucoes.dsvendas.entities.Seller;
+import com.nksolucoes.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+
+}
